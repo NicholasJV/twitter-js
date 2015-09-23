2048ab7ccdc6f4ae483b4c79c86804b6bee94981
NicholasJV <nick@Nicks-MBP.local>
2015-09-23 10:12:01 -0400

    some last work to sync before day 2

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var app = express();
 var morgan = require("morgan");
+var swig = require("swig")
 
 
-
+var data = {
+	title: 'An Example',
+	people: [{
+		name: 'ScottEggs'
+	}, {
+		name: 'NickVan'
+	}, {
+		name: 'Samson'
+	}, {
+		name: 'D\'Angelo'
+	}]
+};
+swig.renderFile(__dirname + '/views/index.html', data, function (err, output) {
+	console.log(output)
+})
 
 // app.use(function(req, res, next){
 // 	// res.send("Sup use func!");
@@ -17,13 +32,20 @@ var morgan = require("morgan");
 app.use(morgan('tiny'));
 
 
-
 app.get('/', function(req, res){
 	res.send("Sup world mon, cool runnings!");
 })
 
 app.get('/option', function(req, res){
 	res.send("Here's a useless corner")
+})
+
+app.get('/option2', function(req, res){
+	var someHtml = swig.renderFile(__dirname + '/views/index.html', data, function (err, output) {
+		console.log(output);
+		return output;
+ 	})
+	res.send(someHtml);
 })
 
 app.post('/', function(req, res){
